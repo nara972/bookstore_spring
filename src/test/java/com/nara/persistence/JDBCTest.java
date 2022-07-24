06f484f1e1a749f6923cdbcecaf6053afafa4bdd
nara972 <68370958+nara972@users.noreply.github.com>
2022-07-24 21:11:45 +0900

    Initial commit

---
+++ src/test/java/com/nara/persistence/JDBCTest.java
@@ -0,0 +1,36 @@
+package com.nara.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class JDBCTest {
+	
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try(Connection con = 
+				DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/blog?useSSL=false&serverTimezone=Asia/Seoul",
+						"root",
+						"mysql")){
+			System.out.println(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+	}
+	
+	
+}
